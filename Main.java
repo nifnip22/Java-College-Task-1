92c1e45fd0827e47c53ba132bb1950baf8a78d86
nifnip22 <proxigaming742@gmail.com>
2024-11-26 20:11:47 +0800

    Add Discount

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int harga_A = 500000;
-        int harga_B = 400000;
-        int harga_C = 300000;
+        float harga_A = 500000;
+        float harga_B = 400000;
+        float harga_C = 300000;
 
         try (Scanner input = new Scanner(System.in)) {
             while (true) {
@@ -46,24 +46,38 @@ public class Main {
         }
     }
 
-    public static void cetakDetail(String kode, String nama, int harga, Scanner input) {
+    public static void cetakDetail(String kode, String nama, float harga, Scanner input) {
         try {
             System.out.print("Masukkan Quantity atau Jumlah Barang : ");
             int quantity = input.nextInt();
             input.nextLine();
             System.out.println("");
 
-            int totalHarga = harga * quantity;
+            float totalHarga = harga * quantity;
+            String discountDisplay = "0%";
+            float discount = 0, jumlahBiaya = totalHarga;
+
+            if (totalHarga >= 2000000) {
+                discountDisplay = "10%";
+                discount = totalHarga * 10 / 100;
+                jumlahBiaya = totalHarga - discount;
+            } else if (totalHarga >= 1500000) {
+                discountDisplay = "5%";
+                discount = totalHarga * 5 / 100;
+                jumlahBiaya = totalHarga - discount;
+            }
             
-            System.out.printf("|-----------------------------------------------|\n");
-            System.out.printf("|            DETAIL SETELAH PERHITUNGAN         |\n");
-            System.out.printf("|-----------------------------------------------|\n");
-            System.out.printf("| %-30s : %-12s |\n", "Kode Barang (Nama Barang)", kode + " (" + nama + ")");
-            System.out.printf("| %-30s : %-12s |\n", "Harga Satuan Barang", "Rp. " + String.format("%,d", harga));
-            System.out.printf("| %-30s : %-12d |\n", "Quantity yang diinputkan", quantity);
-            System.out.printf("|-----------------------------------------------|\n");
-            System.out.printf("  %-30s : %-12s  \n", "Total Harga Barang", "Rp. " + String.format("%,d", totalHarga));
-            System.out.printf("|-----------------------------------------------|\n");
+            System.out.printf("-----------------------------------------------\n");
+            System.out.printf("            DETAIL SETELAH PERHITUNGAN         \n");
+            System.out.printf("-----------------------------------------------\n");
+            System.out.printf("  %-30s : %-12s  \n", "Kode Barang (Nama Barang)", kode + " (" + nama + ")");
+            System.out.printf("  %-30s : %-12s  \n", "Harga Satuan Barang", "Rp. " + String.format("%,.2f", harga));
+            System.out.printf("  %-30s : %-12d  \n", "Quantity yang diinputkan", quantity);
+            System.out.printf("-----------------------------------------------\n");
+            System.out.printf("  %-30s : %-12s  \n", "Total Harga Barang", "Rp. " + String.format("%,.2f", totalHarga));
+            System.out.printf("  %-30s : %-12s  \n", "Discount", discountDisplay);
+            System.out.printf("  %-30s : %-12s  \n", "Jumlah Biaya Setelah Diskon", "Rp. " + String.format("%,.2f", jumlahBiaya));
+            System.out.printf("-----------------------------------------------\n");
             System.out.println("");
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
